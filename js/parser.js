9c3f512cb0c0d692dabd379b1d1c17984a8278de
thundersmotch <2165764+ThunderSmotch@users.noreply.github.com>
2019-12-07 02:01:47 +0000

    t

--- js/parser.js
+++ js/parser.js
@@ -1,4 +1,14 @@
 
-$(function(){
-    $("#body").load("test2.html");
-});  
+//On Load check the structure of data folder and dynamically create the nav bar.
+//Click Function for the NavBar elements
+$(document).ready(function(){
+    $("#navbox").load("navbox.html", function(){
+        $("#navbox a").click(function(e){
+            e.preventDefault();
+            $("#content").load(e.target.href);
+            setTimeout(function(){
+            MathJax.typesetPromise();
+        }, 100);
+        });
+    });  
+});
